52a8a6d4d48eec785147ccd352ce1fef465c48c2
Alejandro Acosta <alee.acosta@gmail.com>
2023-01-11 19:44:45 +0100

    useForm

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { CounterApp } from './01-useState/CounterApp'
 import './index.css'
 import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 import { SimpleForm } from './02-useEffect/SimpleForm'
+import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -12,5 +13,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <CounterApp />
     <CounterWithCustomHook />
     <SimpleForm />
+    <FormWithCustomHook />
   </React.StrictMode>,
 )
